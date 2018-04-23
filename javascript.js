466d0aa16d15a9aadf4ec4b93a7455d0c77abb40
Parul Agarwal <polk54@gmail.com>
2018-04-23 16:25:58 -0400

    Add files via upload

--- javascript.js
+++ javascript.js
@@ -3,5 +3,17 @@ function newWord(){
   document.getElementById("wordDisplay").innerHTML = words[randomNumber];
 }
 
-var words = ["Write down one thing to celebrate and let yourself feel that happiness",
-"Put your phone out of reach for one hour", ]
+var words = ["Write down 5 things that make you happy",
+"Put your phone out of reach for one hour",
+"Take a bath - bubbles and all",
+"List three things you appreciate about yourself",
+"Do something outside",
+"Meditate for 10 minutes",
+"Journal about a dream or goal — no matter how unrealistic",
+"Organize one small part of your living space",
+"Go to the library",
+"Go to the park",
+"Take yourself out to a nice dinner",
+"Put on a facemask",
+"Listen to relaxing music - like bossa nova",
+"Go for a long walk",]
